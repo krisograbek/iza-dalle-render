acc451fe86162f048df196b97f18d1934472b40f
krisograbek <krzysztof.ograbek@gmail.com>
2024-04-15 12:29:29 +0200

    start using axios to detect back end

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,24 +1,20 @@
-// Replace this with the actual API URL
-const API_URL = 'http://localhost:5001';
+import axios from 'axios';
+
+const api = axios.create();
 
 export const generateImages = async (prompt, object, isHD, imageAspect) => {
-  const fullPrompt = `${prompt} \n\n[Objects]: ${object}`
-  console.log(fullPrompt)
+  const fullPrompt = `${prompt} \n\n[Objects]: ${object}`;
+  console.log(fullPrompt); // You can remove this after debugging
+
   try {
-    const response = await fetch(`${API_URL}/generate`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ prompt: fullPrompt, hd: isHD, ar: imageAspect }),
+    const response = await api.post('/generate', {
+      prompt: fullPrompt,
+      hd: isHD,
+      ar: imageAspect
     });
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json()
-    console.log('response', data)
-    return data;
+    console.log('response', response.data); // Debugging response
+    return response.data;
   } catch (error) {
     console.error('There has been a problem with your fetch operation:', error);
     throw error;
